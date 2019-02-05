2ffd7e336a349fd7241db84df215dfcb3c58a740
tyler.maran <tyler.maran@gmail.com>
2019-02-05 18:37:58 -0500

    Heroku bug fixing

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 // apparently require('...') is old syntax for js, but still required for node
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || '3000';
 
 const server = http.createServer(app)
 // to create a server we need to pass a listener
