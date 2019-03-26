f7a86d96a3793c6d7a1f26ed4b45df713de5f1c9
tyler.maran <tyler.maran@gmail.com>
2019-03-25 19:45:44 -0700

    Adding a ton of data to the club schema

--- api/models/club.js
+++ api/models/club.js
@@ -1,76 +1,388 @@
-// club name
-// address
-// zip code
-// city
-// state
-// brief description
 // survey results
-// images
-// main image
 // classification
 // Awards
 // ClubCorp member
 // Reviews
 // platinum list
-// Membership director image
 
 const mongoose = require('mongoose');
 
 // make sure to use the mongoose.Schema.whatever when designating the schema
 const clubSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: {
+    name: { // Club Name
         type: String,
         required: true,
         timestamps: true
     },
-    address: {
+    address: { // Street Address
         type: String,
         required: false,
         default: 'Address'
     },
-    city: {
+    city: { // City
         type: String,
         required: false,
         default: 'City'
     },
-    state: {
+    state: { // State
         type: String,
         required: true,
         default: 'State'
     },
-    zipCode: {
+    zipCode: { // Zip Code
         type: String,
         required: false,
         default: 'ZipCode'
     },
-    user: {
+    user: { // Linked user (linked schema)
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: false,
         unique: true
     },
-    images: [{
+    images: [{ // Image (linked schema)
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Image',
         required: false
     }],
-    url: {
+    url: { // Club URL
         type: String,
         required: false,
         default: 'Empty'
     },
-    profilePic: {
+    profilePic: { // Profile Picture
         type: String,
         required: true,
         default: 'https://via.placeholder.com/200'
     },
-    description: {
+    description_mini: { // Mini Description (first 100ish characters of description)
         type: String,
-        required: false,
-        default: 'An undescribable club experience!'
+        required: true,
+        default: 'An indescribable club experience!'
+    },
+    description: { // Description
+        type: String,
+        required: true,
+        default: 'An indescribable club experience!'
+    },
+    membership_director: {
+        md_name: {
+            type: String,
+            required: true,
+            default: null
+        },
+        md_description: {
+            type: String,
+            required: true,
+            default: null
+        },
+        md_url: {
+            type: String,
+            required: true,
+            default: null
+        },
+        md_profile_picture: {
+            type: String,
+            required: true,
+            default: 'https://via.placeholder.com/200'
+        }
+    },
+    facilities: { // All club facilities - default populated with null 
+        facilities_golf: { // Golf 
+            name: {
+                type: String,
+                required: true,
+                default: 'Golf'
+            },
+            exists: {
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            description: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url_title: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url: {
+                type: String,
+                required: true,
+                default: null
+            },
+            private_course: {
+                type: Boolean,
+                required: true,
+                default: true
+            },
+            number_holes: {
+                type: Number,
+                required: true,
+                default: 9
+            },
+            par: {
+                type: Number,
+                required: true,
+                default: null
+            },
+            length: {
+                type: Number,
+                required: true,
+                default: null
+            },
+            slope: {
+                type: Number,
+                required: true,
+                default: null
+            },
+            driving_range: {
+                type: Boolean,
+                required: true,
+                default: null
+            },
+            year_built: {
+                type: Number,
+                required: true,
+                default: null
+            },
+            golf_season: {
+                type: String,
+                required: true,
+                default: 'Year Round'
+            },
+            cart_rental: {
+                type: Boolean,
+                required: true,
+                default: null
+            },
+            club_rental: {
+                type: Boolean,
+                required: true,
+                default: null
+            }
+        },
+        facilities_tennis: { // Tennis
+            name: {
+                type: String,
+                required: true,
+                default: 'Tennis'   
+            },
+            exists: {
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            description: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url_title: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url: {
+                type: String,
+                required: true,
+                default: null
+            },
+        },
+        facilities_fitness: { // Fitness
+            name: {
+                type: String,
+                required: true,
+                default: 'Fitness'
+            },
+            exists: {
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            description: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url_title: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url: {
+                type: String,
+                required: true,
+                default: null
+            },
+        },
+        facilities_dining: { // Dining
+            name: {
+                type: String,
+                required: true,
+                default: 'Dining'
+            },
+            description: {
+                type: String,
+                required: true,
+                default: null
+            },
+            exists: {
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            url_title: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url: {
+                type: String,
+                required: true,
+                default: null
+            }
+        },
+        facilities_kids: { // Kids
+            name: {
+                type: String,
+                required: true,
+                default: 'Kids Facilities'
+            },
+            description: {
+                type: String,
+                required: true,
+                default: null
+            },
+            exists: {
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            url_title: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url: {
+                type: String,
+                required: true,
+                default: null
+            }
+        },
+        facilities_reciprocal: { // Reciprocal Memberships
+            name: {
+                type: String,
+                required: true,
+                default: 'Reciprocal Memberships'
+            },
+            description: {
+                type: String,
+                required: true,
+                default: null
+            },
+            exists: {
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            url_title: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url: {
+                type: String,
+                required: true,
+                default: null
+            }
+        },
+        facilities_skiing: { // Skiing (beta)
+            name: {
+                type: String,
+                required: true,
+                default: 'Skiing'
+            },
+            exists: {
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            description: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url_title: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url: {
+                type: String,
+                required: true,
+                default: null
+            }
+        },
+        facilities_sailing: { // Sailing (beta)
+            name: {
+                type: String,
+                required: true,
+                default: 'Sailing'
+            },
+            exists: {
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            description: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url_title: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url: {
+                type: String,
+                required: true,
+                default: null
+            }
+        },
+        facilities_custom: { // Tennis
+            name: {
+                type: String,
+                required: true,
+                default: 'Custom Option'   
+            },
+            exists: {
+                type: Boolean,
+                required: true,
+                default: false
+            },
+            description: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url_title: {
+                type: String,
+                required: true,
+                default: null
+            },
+            url: {
+                type: String,
+                required: true,
+                default: null
+            },
+        } 
     }
-
 });
 
 // We want to export the schema wrapped in a model that contains our schema
