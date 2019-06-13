b09a2dd214c7e8e81ab9774cd5af7b37ce509516
tyler.maran <tyler.maran@gmail.com>
2019-06-13 15:34:55 -0700

    cors sucks

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ mongoose.set('useCreateIndex', true);
 const cors = require('cors');
 
 var corsOptions = {
-    origin: 'http://localhost:3000' || 'http://clubfinder.us-east-2.elasticbeanstalk.com/',
+    // origin: 'http://localhost:3000'
+    origin: 'http://clubfinder.us-east-2.elasticbeanstalk.com/',
     credentials: true,
 }
 
